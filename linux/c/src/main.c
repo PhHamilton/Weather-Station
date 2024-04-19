5f18116c5f8ce87acde75254580a0ffc1cd476f7
Philip Hamilton <philip.h.hamilton@gmail.com>
2024-04-19 22:08:59 +0200

    New main for testing bmp integration

--- linux/c/src/main.c
+++ linux/c/src/main.c
@@ -1,15 +1,24 @@
+#include <stdio.h>
 #include <stdint.h>
-#include "i2c_handler.h"
+#include "bmp280.h"
 
-#define BMP280_ADDR 76
+#define BMP280_ADDR 0x76
 
 int main(int argc, char** argv)
 {
-	char *I2C_adapter = "i2c-1";
+	BMP280_CONFIG cfg = {.device_addr = BMP280_ADDR, .I2C_adapter = "i2c-1", .mode = FORCED};
+
+	if(initialize_BMP280(&cfg) != BMP280_OK)
+		printf("Failed to initialize BMP280\n");
+	else
+		printf("BMP280 initialized successfully!\n");
+/*
 	if(initialize_i2c(I2C_adapter) != I2C_OK)
 	{
 		printf("Failed to initialize I2C\n");
 	}
+	else
+		printf("I2C initialized successfully!\n");
 
 	uint8_t data_buf;
 	uint8_t rc = read_reg(BMP280_ADDR, 0xD0, (uint8_t *)&data_buf, 1);
@@ -17,7 +26,8 @@ int main(int argc, char** argv)
 	if(rc != I2C_OK)
 		printf("Failed to read reg: %i\n", rc);
 	else
-		printf("%i\n", data_buf);
+		printf("Successfully read register: %i\n", data_buf);
+*/
 
 	return 0;
 }
